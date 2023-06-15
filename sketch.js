f24df030113784e0b88816b93d30bcea6a7eb733
turinfox <133285763+turinfox@users.noreply.github.com>
2023-06-14 18:11:19 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+
+var turin;
+
+
+function setup() {
+ createCanvas(1200,600);
+ turin=createSprite(600,300);
+}
+
+function draw() {
+    background("black");
+   // rect(600,300,100,100);
+      drawSprites();
+}
